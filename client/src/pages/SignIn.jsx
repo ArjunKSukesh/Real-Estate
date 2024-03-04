7d5f64f88d20a3f16a781a44b4a41bbe02eb36cc
Arjun k <arjunksukesh99@gmail.com>
2024-03-04 07:49:08 +0530

    complete sign in page functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,89 @@
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 export default function SignIn() {
-    return(
-        <div>
-            SignIn
+    const [formData, setFormData] = useState({})
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(false)
+    const navigate = useNavigate();
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.id]: e.target.value
+        })
+    }
+    console.log(formData)
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            setLoading(true);
+            const res = await fetch('/api/auth/signin', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData)
+            });
+
+            const data = await res.json();
+            console.log(data)
+
+            if (data.success === false) {   //susccess is from the backend middleware(index.js)
+                setLoading(false)
+                setError(data.message);
+                return;
+            }
+            setLoading(false)
+            setError(null)
+            navigate('/')
+        } catch (error) {
+            setLoading(false)
+            setError(error.message)
+        }
+    }
+
+    return (
+        <div className="p-3 max-w-lg mx-auto">
+            <h1 className="text-center text-3xl my-7 font-semibold">Sign In</h1>
+            <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+
+
+                <input
+                    type="text"
+                    className="border p-3 rounded-lg"
+                    placeholder="email"
+                    id="email"
+                    onChange={handleChange} />
+
+                <input
+                    type="text"
+                    className="border p-3 rounded-lg"
+                    placeholder="password"
+                    id="password"
+                    onChange={handleChange} />
+
+                <button
+                    disabled={loading}
+                    type='submit'
+                    className="bg-slate-700 text-white p-3 rounded-lg 
+                    hover:opacity-95 disabled:opacity-80 uppercase"
+                >
+                    {loading ? 'Loading...' : 'sign in'}
+                </button>
+
+            </form>
+            <div className='flex gap-1 mt-4'>
+                <p>Dont have an account? </p>
+                <Link to={'/sign-up'}>
+                    <p className='text-blue-700'>Sign up</p>
+                </Link>
+            </div>
+            {error && <p className='text-red-700'>{error}</p>}
         </div>
     )
 }
+
+
+
+
--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -30,7 +30,7 @@ export const signin = async (req, res, next) => {
         if (!validUser) return next(errorHandler(404, 'User not found'))
         // passowrd is compared with password in the database using bcryptjs.compareSync
         const validPassword = bcryptjs.compareSync(password, validUser.password);
-        if (!validPassword) return next(errorHandler(404, 'wrong credential!'))
+        if (!validPassword) return next(errorHandler(404, 'wrong credentials!'))
         //cookie
         const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
         const{password :pass, ...rest} = validUser._doc; // by using ._doc to the validUser it will return all the details except password
