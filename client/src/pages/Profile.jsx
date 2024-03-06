39f24dd1be78517681fd41998d8697f3cad67c12
Arjun k <arjunksukesh99@gmail.com>
2024-03-06 16:11:57 +0530

    complete image upload functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,16 +1,74 @@
 import { useSelector } from 'react-redux'
+import { useRef, useState, useEffect } from 'react'
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage'
+import { app } from '../firebase'
 
 export default function Profile() {
-    const { currentUser } = useSelector(state => state.user)
+    const { currentUser } = useSelector(state => state.user);
+    const fileRef = useRef(null);
+    const [file, setFile] = useState(undefined);
+    const [filePerc, setFilePerc] = useState(0);
+    const [fileUploadError, setFileUploadError] = useState(false);
+    const [formData, setFormData] = useState({});
+    // console.log(formData);
+    // console.log(filePerc);
+    // console.log(fileUploadError);
+
+    useEffect(() => {
+        if (file) {
+            handleFileUpload(file);
+        }
+    }, [file]);
+
+    const handleFileUpload = () => {
+        //create storage
+        const storage = getStorage(app);
+        // create filename with time, so it will be always unique
+        const fileName = new Date().getTime() + file.name;
+        //create storage reference showing which place to store the storage
+        const storageRef = ref(storage, fileName);
+        //uploadTask is used to show the percentage of upload
+        const uploadTask = uploadBytesResumable(storageRef, file);
+
+        uploadTask.on('state_changed', (snapshot) => {
+            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            // console.log('Upload is '+ progress+'% done ')
+            setFilePerc(Math.round(progress));
+        },
+            (error) => {
+                setFileUploadError(true);
+            },
+            () => {
+                getDownloadURL(uploadTask.snapshot.ref)
+                    .then((downloadURL) => setFormData({ ...formData, avatar: downloadURL }));
+            });
+    }
     return (
         <div className='p-3 max-w-lg mx-auto'>
-            <h1 className="text-3xl text-center my-7">Profile</h1>
+            <h1 className="text-3xl font-bold text-center my-7">Profile</h1>
 
             <form className='flex flex-col gap-3'>
+                <input
+                    onChange={(e) => setFile(e.target.files[0])}
+                    type="file"
+                    ref={fileRef}
+                    accept='image/*'
+                    hidden />
                 <img
+                    onClick={() => fileRef.current.click()}
                     className='mt-2 self-center rounded-full h-24 w-24 object-cover cursor-pointer'
-                    src={currentUser.avatar}
+                    src={formData.avatar ||currentUser.avatar}
                     alt="prfile" />
+                <p className='text-sm self-center'>
+                {fileUploadError ? (
+                    <span className='text-red-700'>Error Image Upload</span>
+                ) : filePerc > 0 && filePerc < 100 ? (
+                    <span className='text-slate-700'>{`Uploading ${filePerc}%`}</span>
+                ) : filePerc === 100 ? (
+                    <span className='text-green-700'>Image Successfully Uploaded</span>
+                ) : ("")
+                }
+                </p>
 
                 <input
                     className='p-3 rounded-lg border'
@@ -27,9 +85,9 @@ export default function Profile() {
                     type='text'
                     id='password'
                     placeholder='password' />
-                    <button 
-                    className='bg-slate-700 text-white p-3 rounded-lg hover:opacity-95 disabled:opacity-80 uppercase' 
-                    >update</button>
+                <button
+                    className='bg-slate-700 text-white p-3 rounded-lg hover:opacity-95 disabled:opacity-80 uppercase'
+                >update</button>
             </form>
             <div className='flex justify-between mt-5'>
                 <span className='text-red-700 cursor-pointer'>Delete Account</span>
